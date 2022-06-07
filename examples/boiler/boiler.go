1d2dd5aebf7ac6e3d8c5bc513222ca0e78f1deac
ugabiga <ugabiga@gmail.com>
2022-06-07 16:15:46 +0900

    Add sqlboiler seed

--- examples/boiler/boiler.go
+++ examples/boiler/boiler.go
@@ -3,12 +3,14 @@ package boiler
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"github.com/ugabiga/go-orm-example/config"
 	"github.com/ugabiga/go-orm-example/examples/boiler/models"
 	"github.com/ugabiga/go-orm-example/internal"
 	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"math/rand"
 	"time"
 )
 
@@ -18,14 +20,51 @@ func Execute() {
 	conn, err := sql.Open(config.GetDBInfo())
 	internal.LogFatal(err)
 
-	crudExamples(ctx, conn)
+	crud(ctx, conn)
 	queryWithRelation(ctx, conn)
+	seed(100, ctx, conn)
 
 	// Aggregation
 	// Pagination
 	// Transform
 	// Raw Query
 	// Event
+}
+
+func seed(count int, ctx context.Context, conn *sql.DB) {
+	if count == 0 {
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+
+	var newUsers []*models.User
+	for i := 0; i < count; i++ {
+		newUser := &models.User{
+			FirstName: "Sample",
+			LastName:  "Unknown",
+			Birthday:  time.Now().AddDate(-30, 0, 0),
+		}
+		err := newUser.Insert(ctx, conn, boil.Infer())
+		internal.LogFatal(err)
+
+		newUsers = append(newUsers, newUser)
+	}
+
+	var newTasks []*models.Task
+	for i := 0; i < count; i++ {
+		num := i + 1
+		newTask := &models.Task{
+			UserID: null.Int64{Int64: newUsers[rand.Intn(len(newUsers))].ID, Valid: true},
+			Title:  fmt.Sprintf("Task %d", num),
+			Note:   fmt.Sprintf("Note %d", num),
+			Status: models.TaskStatusTodo,
+		}
+		err := newTask.Insert(ctx, conn, boil.Infer())
+		internal.LogFatal(err)
+
+		newTasks = append(newTasks, newTask)
+	}
 }
 
 func queryWithRelation(ctx context.Context, conn *sql.DB) {
@@ -65,7 +104,7 @@ func queryWithRelation(ctx context.Context, conn *sql.DB) {
 	internal.PrintJSONLog(gotTask.R.User)
 }
 
-func crudExamples(ctx context.Context, conn *sql.DB) {
+func crud(ctx context.Context, conn *sql.DB) {
 	// Create
 	newUser := &models.User{
 		FirstName: "Sample",
