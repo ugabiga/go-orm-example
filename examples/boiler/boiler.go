a8e1d3fa83302259738f06cc3a4ff450caff3f96
ugabiga <ugabiga@gmail.com>
2022-06-07 15:46:07 +0900

    Add sqlboiler query with relation examples

--- examples/boiler/boiler.go
+++ examples/boiler/boiler.go
@@ -6,7 +6,9 @@ import (
 	"github.com/ugabiga/go-orm-example/config"
 	"github.com/ugabiga/go-orm-example/examples/boiler/models"
 	"github.com/ugabiga/go-orm-example/internal"
+	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 	"time"
 )
 
@@ -16,13 +18,61 @@ func Execute() {
 	conn, err := sql.Open(config.GetDBInfo())
 	internal.LogFatal(err)
 
+	crudExamples(ctx, conn)
+	queryWithRelation(ctx, conn)
+
+	// Aggregation
+	// Pagination
+	// Transform
+	// Raw Query
+	// Event
+}
+
+func queryWithRelation(ctx context.Context, conn *sql.DB) {
+	// Create with relationship
+	newUser := &models.User{
+		FirstName: "Sample",
+		LastName:  "User",
+		Birthday:  time.Now().AddDate(-30, 0, 0),
+	}
+	err := newUser.Insert(ctx, conn, boil.Infer())
+	internal.LogFatal(err)
+	internal.PrintJSONLog(newUser)
+
+	newTask := &models.Task{
+		UserID: null.Int64{Int64: newUser.ID, Valid: true},
+		Title:  "task 1",
+		Note:   "note 1",
+		Status: models.TaskStatusTodo,
+	}
+	err = newTask.Insert(ctx, conn, boil.Infer())
+	internal.LogFatal(err)
+	internal.PrintJSONLog(newTask)
+
+	// Read with relationship
+	gotTask, err := models.Tasks().One(ctx, conn)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(gotTask)
+
+	gotTaskUser, err := gotTask.User().One(ctx, conn)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(gotTaskUser)
+
+	// Eager Loading
+	gotTask, err = models.Tasks(qm.Load(models.TaskRels.User)).One(ctx, conn)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(gotTask)
+	internal.PrintJSONLog(gotTask.R.User)
+}
+
+func crudExamples(ctx context.Context, conn *sql.DB) {
 	// Create
 	newUser := &models.User{
 		FirstName: "Sample",
 		LastName:  "Man",
 		Birthday:  time.Now().AddDate(-30, 0, 0),
 	}
-	err = newUser.Insert(ctx, conn, boil.Infer())
+	err := newUser.Insert(ctx, conn, boil.Infer())
 	internal.LogFatal(err)
 	internal.PrintJSONLog(newUser)
 
@@ -55,12 +105,4 @@ func Execute() {
 	deleteRowsAff, err := models.Users().DeleteAll(ctx, conn)
 	internal.LogFatal(err)
 	internal.PrintJSONLog(deleteRowsAff)
-
-	// CRUD with Relationship
-	// Eager Loading
-	// Aggregation
-	// Pagination
-	// Transform
-	// Raw Query
-	// Event
 }
