bdc3245997c7ad71d8825d8daed600c0ce32efa7
ugabiga <ugabiga@gmail.com>
2022-06-09 20:31:33 +0900

    Add entgo query with relation

--- example/ente/ente.go
+++ example/ente/ente.go
@@ -10,6 +10,8 @@ import (
 	"fmt"
 	"github.com/ugabiga/go-orm-example/config"
 	"github.com/ugabiga/go-orm-example/example/ente/ent"
+	"github.com/ugabiga/go-orm-example/example/ente/ent/task"
+	"github.com/ugabiga/go-orm-example/example/ente/ent/user"
 	"github.com/ugabiga/go-orm-example/internal"
 	"log"
 	"time"
@@ -55,10 +57,10 @@ func Execute() {
 	crud(ctx, conn)
 	fmt.Println()
 
-	//fmt.Println("Run QueryWithRelation")
-	//queryWithRelation(ctx, conn)
-	//fmt.Println()
-	//
+	fmt.Println("Run QueryWithRelation")
+	queryWithRelation(ctx, conn)
+	fmt.Println()
+
 	//fmt.Println("Run Seed")
 	//seed(ctx, conn, 100)
 	//fmt.Println()
@@ -84,7 +86,44 @@ func Execute() {
 	//fmt.Println()
 }
 
+func queryWithRelation(ctx context.Context, c *ent.Client) {
+	// Create with relationship
+	newUser, err := c.User.Create().
+		SetFirstName("Sample").
+		SetLastName("User").
+		SetBirthday(time.Now().AddDate(-30, 0, 0)).
+		Save(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(newUser)
+
+	newTask, err := c.Task.Create().
+		SetUser(newUser).
+		SetTitle("task 1").
+		SetNote("note 1").
+		SetStatus(task.StatusTodo).
+		Save(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(newTask)
+
+	// Read with relationship
+	gotTask, err := c.Task.Get(ctx, newTask.ID)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(gotTask)
+
+	gotTaskUser, err := gotTask.QueryUser().First(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(gotTaskUser)
+
+	// Eager Loading
+	tasks, err := c.Task.Query().
+		WithUser().
+		All(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(tasks)
+}
+
 func crud(ctx context.Context, c *ent.Client) {
+	// Create
 	newUser, err := c.User.
 		Create().
 		SetFirstName("Sample").
@@ -93,6 +132,33 @@ func crud(ctx context.Context, c *ent.Client) {
 		Save(ctx)
 	internal.LogFatal(err)
 	internal.PrintJSONLog(newUser)
+
+	// Read
+	gotUser, err := c.User.
+		Query().
+		Where(user.FirstNameEQ("Sample")).
+		First(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(gotUser)
+
+	// Read List
+	gotUsers, err := c.User.Query().Where(user.FirstNameEqualFold("Sample")).All(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(gotUsers)
+
+	// Update
+	updateRowsAff, err := c.User.Update().
+		Where(user.LastNameEQ("User")).
+		SetLastName("Unknown").
+		Save(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(updateRowsAff)
+
+	// Delete
+	deleteRowsAff, err := c.User.Delete().
+		Exec(ctx)
+	internal.LogFatal(err)
+	internal.PrintJSONLog(deleteRowsAff)
 }
 
 func makeConnection() *ent.Client {
