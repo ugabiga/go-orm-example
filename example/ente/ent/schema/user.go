9e5b912205b20d56f803b4ecde98470510be92fb
ugabiga <ugabiga@gmail.com>
2022-06-09 16:20:25 +0900

    Add entgo and define basic models

---
+++ example/ente/ent/schema/user.go
@@ -0,0 +1,40 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"time"
+)
+
+// User holds the schema definition for the User entity.
+type User struct {
+	ent.Schema
+}
+
+// Fields of the User.
+func (User) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("first_name"),
+		field.String("last_name"),
+		field.Time("birthday").
+			Optional().
+			SchemaType(map[string]string{
+				dialect.Postgres: "DATE",
+			}),
+		field.Time("updated_at").
+			Default(time.Now).
+			UpdateDefault(time.Now),
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable(),
+	}
+}
+
+// Edges of the User.
+func (User) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("tasks", Task.Type),
+	}
+}
---
+++ example/ente/ente.go
@@ -0,0 +1,5 @@
+package ente
+
+func Execute() {
+
+}
